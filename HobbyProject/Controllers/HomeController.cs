ba53f28ded908e216821b408c2bd649d4e82ef94
j1010756 <106291794+j1010756@users.noreply.github.com>
2024-02-06 16:51:55 -0700

    Add project files.

---
+++ HobbyProject/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using HobbyProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HobbyProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        // change name to calculator to match project requirements
+        public IActionResult Calculator()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HobbyProject/wwwroot/js/site.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+
+    // do calculation when button is pressed
+    $('#calculate').click(function () {
+
+        // get number of hours
+        var hours = parseFloat($('#hours').val());
+
+        // validate number
+        if (hours < 0.1 || isNaN(hours)) {
+            alert('Please enter a positive number for hours.');
+            return;
+        }
+
+        // get hourly rate
+        var hourlyRate = parseFloat($('#hourlyRate').val().replace('$', ''));
+
+        // determine total cost
+        var totalCost = hours * hourlyRate;
+
+        // set value of output to the total cost
+        $('#output').val('$' + totalCost.toFixed(2));
+    });
+});
